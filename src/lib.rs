73689dd1d8a8946ca94c42e0ff9c9280726bf22f
Nimna Pathum <113501913+Nimnapathum@users.noreply.github.com>
2024-06-24 11:35:55 +0530

    upload structs handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,26 @@
-pub use crate::product::Product;
-pub use crate::product::Category;
-pub use crate::customer::Customer;
-pub use crate::order::Order;
+#[derive(Debug , Default)]
+pub struct Student{
+    pub id: u8,
+    pub age: u8,
+    pub name: String,
+}
 
-mod product{
-    pub use category::Category;
-    pub struct Product {
-        id: u64,
-        name: String,
-        price: f64,
-        category: Category,
-    }
-    
-    mod category{
-        pub enum Category{
-            Electronics,
-            Clothing,
-            Books,
-        }
-    }
-      
-    impl Product{
-        fn calculate_tax(&self) -> f64{
-            self.price * 0.1
-        }
-    
-        pub fn product_price(&self) -> f64{
-            self.price + self.calculate_tax()
-        }
-
-        pub fn new(id: u64 , name: String , price: f64 , category: Category) -> Product{
-            Product { 
-                id, 
-                name,
-                price, 
-                category
-            }
-        }
+impl Student{
+    pub fn new(name: String) -> Result<Self , String>{
+        if name.chars().all(|x| matches!(x , 'a'..='z')){
+            Ok(Self{
+                id: 0,
+                age: 0,
+                name: name ,
+            })
+        }else{
+            Err("The name is invalid".to_string())
+        }       
     }
 }
 
-mod customer{
-    pub struct Customer{
-        id: u64,
-        name: String,
-        email: String,
-    }
-    
-    impl Customer{
-        pub fn new(id: u64 , name: String , email: String) -> Customer{
-            Customer{
-                id,
-                name,
-                email
-            }
-        }
-    }
-}
-
-mod order{
-    use crate::product::Product;
-    use crate::customer::Customer;
-    pub struct Order{
-        id: u64,
-        product: Product,
-        customer: Customer,
-        quantity: u32,
-    }
-    
-    impl Order{
-        fn calculate_discount(&self) -> f64{
-            if self.quantity > 5 {
-                0.1
-            }else{
-                0.0
-            }
-        }
-    
-        pub fn total_bill(&self) -> f64{
-            let discount = self.calculate_discount();
-            let total_before_discount = self.product.product_price() * self.quantity as f64;
-            total_before_discount - (total_before_discount * discount)
-        }
-
-        pub fn new(id: u64 , product: Product , customer: Customer , quantity: u32) -> Order{
-            Order { 
-                id, 
-                product, 
-                customer, 
-                quantity
-            }
-        }
-    }
-}
+// impl Default for Student{
+//     fn default() -> Self{
+//         Self { id: 0, age: 20, name: "Unknown".to_string() }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::default;
 
-fn main(){
+use structs::Student;
+fn main() {
+    let std1 = Student::new("nimna".to_string()).unwrap_or_default();
+    println!("{:?}" , std1);
 
+    let std2 = Student::default();
+    println!("{:?}" , std2);
+
+    let std3 = Student{
+        age: 21,
+        ..Default::default()
+    };
+    println!("{:?}" , std3);
 }
